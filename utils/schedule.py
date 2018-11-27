2b98a9cade86fdbe402e7ba8ef8db3ba56f992ed
yutang2 <yutang2@illinois.edu>
2018-11-27 01:43:25 -0600

    add docstrings

--- utils/schedule.py
+++ utils/schedule.py
@@ -4,24 +4,42 @@ import threading
 import time
 
 class ScheduleThread(threading.Thread):
+    """A wrapper class to run the schedule in a separate thread
+
+    This class is implemented as a Thread, with ....
+
+    Attributes:
+        scheduler: Default scheduler from the schedule library
+        cease_continuous_run: An event serve as a stop flag
+    """
     def __init__(self):
+        """Init ScheduleThread class"""
         super(ScheduleThread, self).__init__()
         self.scheduler = schedule
         self.cease_continuous_run = threading.Event()
 
     def run(self):
+        """The run protocol for Thread"""
         while not self.cease_continuous_run.is_set():
             self.scheduler.run_pending()
             time.sleep(1)
     
     def stop(self):
+        """Stop the thread"""
         self.cease_continuous_run.set()
     
     @property
     def add(self):
+        """Helper method for adding task"""
         return self.scheduler
 
 def catch_exceptions(job_func, cancel_on_failure=False):
+    """Decorator for the catch exception utility in scheduler
+    Args:
+        job_func: the original function being wrapped
+        cancel_on_failure: if set to True, job will be canceled
+            if an exception occured
+    """
     @wraps(job_func)
     def wrapper(*args, **kwargs):
         try:
@@ -34,6 +52,7 @@ def catch_exceptions(job_func, cancel_on_failure=False):
     return wrapper
 
 def with_logging(func):
+    """Decorator for the logging utility in scheduler"""
     @wraps(func)
     def wrapper(*args, **kwargs):
         print('LOG: Running job "%s"' % func.__name__)
